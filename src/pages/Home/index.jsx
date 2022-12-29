b53a9e78d729e6223dd601df6dd522281c891b1f
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-12-29 19:19:07 +0000

    list created

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,11 +4,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import Balance from '../../components/Balance';
 import Header from '../../components/Header';
 
+const mockList = [
+  {
+    id: 1,
+    label: 'Cinema Tickets',
+    value: '30,67',
+    date: '13/09/2022',
+    type: 0, //type 0 = expenses
+  },
+  {
+    id: 2,
+    label: 'Salary payment',
+    value: '3210,55',
+    date: '15/09/2022',
+    type: 1, //type 1 = entrances?
+  },
+  {
+    id: 3,
+    label: 'Bike bought',
+    value: '200,67',
+    date: '19/09/2022',
+    type: 0,
+  },
+];
+
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header username="Nome Teste" />
-      <Balance balance="10.000,00" expenses="-555,03"/>
+      <Balance balance="10.000,00" expenses="-555,03" />
+      <Text style={styles.title}>Last records</Text>
     </View>
   );
 }
@@ -17,5 +42,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fafafa',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
   },
 });
