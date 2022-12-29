00c35077a92a8b128dafa1e2327f34bee7658a93
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-12-29 19:55:52 +0000

    skeleton created

--- src/components/Movements/index.jsx
+++ src/components/Movements/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Movements({ data }) {
+  const [showValue, setShowValue] = useState(false);
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={() => setShowValue(!showValue)}>
       <Text style={styles.date}>{data.date}</Text>
       <View style={styles.content}>
         <Text style={styles.label}>{data.label}</Text>
-        <Text style={data.type === 1 ? styles.value : styles.expenses}>
-          {data.type === 1 ? `£ ${data.value}` : `£ -${data.value}`}
-        </Text>
+        {showValue ? (
+          <Text style={data.type === 1 ? styles.value : styles.expenses}>
+            {data.type === 1 ? `£ ${data.value}` : `£ -${data.value}`}
+          </Text>
+        ) : (
+          <View style={styles.skeleton}></View>
+        )}
       </View>
     </TouchableOpacity>
   );
@@ -38,4 +44,5 @@ const styles = StyleSheet.create({
   },
   value: { color: '#2ecc71', fontWeight: 'bold', fontSize: 16 },
   expenses: { color: '#e74c3c', fontWeight: 'bold', fontSize: 16 },
+  skeleton: { marginTop: 6, width: 80, height: 10, backgroundColor: '#dadada', borderRadius: 8 },
 });
